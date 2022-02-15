e919ccbf6e7996322ad00fca70f6df8acab85cd7
mustafa1390 <mustafa1390@gmail.com>
2022-02-15 22:20:31 +0330

    version 1.0.1

---
+++ routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+
+ //Clear route cache
+ Route::get('/route-cache', function() {
+    Artisan::call('route:cache');
+    return 'Routes cache cleared';
+});
+
+//Clear config cache
+Route::get('/config-cache', function() {
+    Artisan::call('config:cache');
+    return 'Config cache cleared';
+});
+
+// Clear application cache
+Route::get('/clear-cache', function() {
+    Artisan::call('cache:clear');
+    return 'Application cache cleared';
+});
+
+// Clear view cachee
+Route::get('/view-clear', function() {
+    Artisan::call('view:clear');
+    return 'View cache cleared';
+});
+
+// Clear cache using reoptimized class
+Route::get('/optimize-clear', function() {
+    Artisan::call('optimize:clear');
+    return 'View cache cleared';
+});
+
+// composer dump-autoload
+Route::get('/updateapp', function() {
+
+    exec('composer dump-autoload');
+    echo 'composer dump-autoload complete';
+});
